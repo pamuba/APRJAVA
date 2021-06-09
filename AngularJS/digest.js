5ff1d18ed56152b23709f2ab2f3375887597699d
pamuba <partha.bora24@gmail.com>
2021-06-09 19:28:56 +0530

    82

--- AngularJS/digest.js
+++ AngularJS/digest.js
@@ -3,4 +3,19 @@ app.controller('emp', function($scope, $rootScope){
 
     $scope.a = 1;
     $scope.b = 2;
+    $scope.s = 0;
+
+    $scope.calcSum = function(){
+        $scope.s = Number($scope.a) + Number($scope.b);
+    }
 });
+
+var btnClick = function(){
+    var $scope = angular.element($("#div")).scope();
+    // $scope.s = Number($scope.a) + Number($scope.b);
+    // $scope.$apply();
+
+    $scope.$apply(function(){
+        $scope.s = Number($scope.a) + Number($scope.b);
+    })
+}
