ea42b5d3d938791df4bd17c2bf1333c72164a4d8
pamuba <partha.bora24@gmail.com>
2021-06-10 19:20:49 +0530

    91

--- AngularJS/params.js
+++ AngularJS/params.js
@@ -6,9 +6,30 @@ app.config(function($routeProvider){
         templateUrl:'sumurl.html',
         controller:"sumurl"
     })
+    .when('/suminput', {
+        templateUrl:'suminput.html',
+        controller:"suminput"
+    })
+    .when('/', {
+        template:'<b>Welcome to the APP</b><i>Click on the Links</i>'
+    })
+    .otherwise({
+        template:'<b>No Content Available</b><i>Click on the Links</i>'
+    })
 });
 
 app.controller('sumurl', function($scope, $routeParams){
     $scope.a = $routeParams.a
     $scope.b = $routeParams.b
+})
+
+
+app.controller('suminput', function($scope, $location, $interpolate){
+    $scope.a = 0;
+    $scope.b = 0;
+    $scope.doSum = function(){
+        var url = $interpolate('/sumurl/{{a}}/{{b}}')($scope)
+        console.log(url)
+        $location.path(url);
+    }
 })
