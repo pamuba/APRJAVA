4af9f7206e391303a7aa925062ca6ef7a371168f
pamuba <partha.bora24@gmail.com>
2021-06-10 20:25:47 +0530

    94

--- contactlistapp/public/controller/controller.js
+++ contactlistapp/public/controller/controller.js
@@ -1,3 +1,9 @@
-function AppCtrl(){
+function AppCtrl($scope, $http){
     console.log("Hello from Controller")
+
+    $http.get('/contactlist').success(function(response){
+        // console.log("I got the data", response)
+        $scope.contactlist = response;
+    })
+
 }
--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -3,9 +3,27 @@ var app = express()
 
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', function(req, res){
-//     res.send("Hello World from Server.js")
-// })
+app.get('/contactlist', function(req, res){
+    console.log("received req from controller")
+    person1 = {
+        name: "John",
+        email: "john@gamil.com",
+        number:"(111) 111-1111"
+    };
+    person2 = {
+        name: "Jim",
+        email: "jim@gamil.com",
+        number:"(222) 222-2222"
+    };
+    person3 = {
+        name: "Jill",
+        email: "jill@gamil.com",
+        number:"(333) 333-3333"
+    };
+
+    var contactlist = [person1, person2, person3];
+    res.json(contactlist);
+})
 
 app.listen(9000, function(){
     console.log("Servre running @9000")
