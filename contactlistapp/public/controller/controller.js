322a1280f26a0c6fea4b7939f8d49559dcd14596
pamuba <partha.bora24@gmail.com>
2021-06-11 19:41:23 +0530

    101

--- contactlistapp/public/controller/controller.js
+++ contactlistapp/public/controller/controller.js
@@ -4,6 +4,10 @@ function AppCtrl($scope, $http){
     $http.get('/contactlist').success(function(response){
         // console.log("I got the data", response)
         $scope.contactlist = response;
-    })
+    });
+    $scope.addContact = function(){
+        console.log($scope.contact)
+        $http.post('/contactlist', $scope.contact)
+    }
 
 }
--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -1,28 +1,23 @@
 var express = require('express')
 var app = express()
+var mongojs = require('mongojs')
+var db = mongojs('contactlist', ['contactlist']);
 
+
+app.use(express.json())
 app.use(express.static(__dirname + '/public'));
 
 app.get('/contactlist', function(req, res){
     console.log("received req from controller")
-    person1 = {
-        name: "John",
-        email: "john@gamil.com",
-        number:"(111) 111-1111"
-    };
-    person2 = {
-        name: "Jim",
-        email: "jim@gamil.com",
-        number:"(222) 222-2222"
-    };
-    person3 = {
-        name: "Jill",
-        email: "jill@gamil.com",
-        number:"(333) 333-3333"
-    };
+    
+    db.contactlist.find(function(err, docs){
+        console.log(docs)
+        res.json(docs)
+    })
+})
 
-    var contactlist = [person1, person2, person3];
-    res.json(contactlist);
+app.post('/contactlist', function(req, res){
+    console.log(req.body)
 })
 
 app.listen(9000, function(){
